b0239fba044c34247d1f17fb476bbb9b3c0075f6
Karim tarek <karimtarekabdelaziz@gmail.com>
2021-11-13 22:11:36 +0200

    update project 11/12

--- src/js/main.js
+++ src/js/main.js
@@ -34,4 +34,47 @@ $(function () {
       },
     },
   });
+  // NAVBAR
+  // GO TO SECTION WITH SCROLL
+  const btnToggle = $("#toggle-sidebar");
+  $(".link").on("click", function () {
+    //
+    const goToSection = $(`#${$(this).attr("data-target")}`).offset().top - 30;
+    $("html, body").animate(
+      {
+        scrollTop: goToSection,
+      },
+      500
+    );
+    //
+    if ($(".navbar__items").hasClass("navbar__items--active")) {
+      $(".navbar__items").removeClass("navbar__items--active");
+      $("html, body").css("overflow-y", "visible");
+      $(".backdrop").css("display", "none");
+    }
+  });
+
+  btnToggle.on("click", function () {
+    if ($(".navbar__items").hasClass("navbar__items--active")) {
+      $(".navbar__items").removeClass("navbar__items--active");
+      $(".backdrop").css("display", "none");
+      $("html, body").css("overflow-y", "visible");
+      return;
+    }
+
+    $(".navbar__items").addClass("navbar__items--active");
+    $("html, body").css("overflow-y", "hidden");
+    $(".backdrop").css("display", "block");
+  });
+
+  // CLOSE SIDEBAR NAVBAR IF CLICKED ON BACKDROP
+  $(".backdrop").on("click", function () {
+    btnToggle.trigger("click");
+  });
+
+  // SCROLL ANIMATION
+  AOS.init({
+    duration: 1000,
+    once: true,
+  });
 });
