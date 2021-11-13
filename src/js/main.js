1f38b25c4552df113ff2894963b2a2a3cc8ae7ad
Karim tarek <karimtarekabdelaziz@gmail.com>
2021-11-13 03:00:08 +0200

    update project 11/12

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,35 @@ $(function () {
 
   // SET YEAR
   year.text(new Date().getFullYear());
+
+  // GET DIRECTION HTML
+  const getDir = $("html").attr("dir") === "rtl" ? true : false;
+
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    autoplayHoverPause: true,
+    loop: true,
+    dots: false,
+    nav: true,
+    rtl: getDir,
+    margin: 15,
+    navText: [
+      "<div class='prev-slide'></div>",
+      "<div class='next-slide'></div>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
 });
